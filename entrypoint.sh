b1d8125fd8b459ba2d7d9ebb39e73dcb7f9daed8
mrder <mrder@hotmail.de>
2025-02-27 21:24:45 +0100

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-echo "Starting Lizenzmanager with:"
-echo "USERNAME: $USERNAME"
-echo "BASE_DOMAIN: $BASE_DOMAIN"
-echo "PORT: $PORT"
+# Debugging: Zeige die Umgebungsvariablen beim Start
+echo "### STARTE LIZENZMANAGER MIT FOLGENDEN EINSTELLUNGEN ###"
+env | grep -E "USERNAME|PASSWORD|BASE_DOMAIN|UPLOAD_FOLDER|PORT"
 
-# Starte die Flask-Anwendung
+# Falls eine Variable fehlt, Standardwerte setzen
+export USERNAME=${USERNAME:-admin}
+export PASSWORD=${PASSWORD:-secret1991}
+export BASE_DOMAIN=${BASE_DOMAIN:-http://localhost:5200}
+export UPLOAD_FOLDER=${UPLOAD_FOLDER:-/app/uploads}
+export PORT=${PORT:-5200}
+
+# Starte die Anwendung
 exec python app.py
