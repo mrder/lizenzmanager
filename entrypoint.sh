8ca21aaef1b6368fd0049afaa114e2af99f398e3
mrder <mrder@hotmail.de>
2025-03-13 12:39:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-echo "### STARTE LIZENZMANAGER MIT DIESEN VARIABLEN ###"
+echo "### Starte Lizenzmanager ###"
 env | grep -E "USERNAME|PASSWORD|BASE_DOMAIN|UPLOAD_FOLDER|PORT|SECRET_KEY"
 
 export USERNAME=${USERNAME:-admin}
@@ -10,10 +9,17 @@ export UPLOAD_FOLDER=${UPLOAD_FOLDER:-/app/uploads}
 export PORT=${PORT:-5200}
 export SECRET_KEY=${SECRET_KEY:-123456}
 
-# Setze FLASK_APP, damit Flask-Migrate funktioniert
 export FLASK_APP=app.py
 
-# Führe alle ausstehenden Migrationen aus
+# Falls noch kein Migrationsordner existiert, initialisiere die Migration
+if [ ! -d "migrations" ]; then
+    echo "Migrationsordner nicht gefunden. Führe Initialisierung und erste Migration durch..."
+    flask db init
+    flask db migrate -m "Initial migration"
+fi
+
+# Wende alle Migrationen an
+echo "Führe 'flask db upgrade' aus..."
 flask db upgrade
 
 # Starte die Anwendung
