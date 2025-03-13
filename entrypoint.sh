aa4892af9a4f18f9e1750c388a5a58a5564e2aae
mrder <mrder@hotmail.de>
2025-03-13 11:37:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ env | grep -E "USERNAME|PASSWORD|BASE_DOMAIN|UPLOAD_FOLDER|PORT"
 
 # Falls eine Variable fehlt, Standardwerte setzen
 export USERNAME=${USERNAME:-admin}
-export PASSWORD=${PASSWORD:-secret1991}
-export BASE_DOMAIN=${BASE_DOMAIN:-http://localhost:5200}
+export PASSWORD=${PASSWORD:-admin}
+export BASE_DOMAIN=${BASE_DOMAIN:-https://localhost:5200}
 export UPLOAD_FOLDER=${UPLOAD_FOLDER:-/app/uploads}
 export PORT=${PORT:-5200}
 
