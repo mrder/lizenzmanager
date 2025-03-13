7f069c7fa91884e40a6f625216295d415e8d18dc
mrder <mrder@hotmail.de>
2025-03-13 11:59:27 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 echo "### STARTE LIZENZMANAGER MIT DIESEN VARIABLEN ###"
 env | grep -E "USERNAME|PASSWORD|BASE_DOMAIN|UPLOAD_FOLDER|PORT|SECRET_KEY"
 
-# Falls eine Variable fehlt, Standardwerte setzen
 export USERNAME=${USERNAME:-admin}
 export PASSWORD=${PASSWORD:-admin}
 export BASE_DOMAIN=${BASE_DOMAIN:-http://localhost:5200}
@@ -14,7 +13,7 @@ export SECRET_KEY=${SECRET_KEY:-123456}
 # Setze FLASK_APP, damit Flask-Migrate funktioniert
 export FLASK_APP=app.py
 
-# Führe alle ausstehenden Migrationen aus (die Migrationsskripte müssen im Repository vorhanden sein)
+# Führe alle ausstehenden Migrationen aus
 flask db upgrade
 
 # Starte die Anwendung
